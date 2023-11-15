779b13a8ccd3a665c8dc0d7cdb004778b50504d3
Robert Chheav <robert.chheav@gmail.com>
2023-11-15 15:13:47 +0100

    the page is functionnal

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,15 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+sea_palace = { name: 'Sea Palace', address: 'Oosterdokskade 8, 1011 AE Amsterdam', category: 'chinese' }
+verona = { name: 'Verona Ristorante', address: 'Regulier 7, 1017 BJ Amsterdam', category: 'italian' }
+sapporo = { name: 'Sapporo', address: 'Scheldestraat 99-H, 1078 GJ Amsterdam', category: 'japanese' }
+bistro_alpes = { name: 'Bistro des alpes', address: 'Utrechaat 141, 1017 WE Amsterdam', category: 'french' }
+fritterie = { name: 'La Fritterie', address: 'Mannenkemn 8, 1083 AD Amsterdam', category: 'belgian' }
+
+[sea_palace, verona, sapporo, bistro_alpes, fritterie].each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+  puts "Created #{restaurant.name}"
+end
+puts 'Finished!'
